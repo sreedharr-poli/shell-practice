0c56bc553d0f14ef170d5f383c865e8d9e113d05
Sreedhar Reddy <sreedhar.poli@gmail.com>
2025-09-18 22:02:58 +0530

    second script staging

--- 02-conversation.sh
+++ 02-conversation.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-echo "conversation between sreedhar and prasad"
-echo "Sreedhar:: Hello prasad how are you doing"
-echo "Prasad:: hey sreedhar I am good. hope you so"
-echo "Sreedhar:: Hope you are joined and learning join devops cource"
-echo "Prasad:: yes I just stated"
+echo "Conversation between Sreedhar and Prasad"
+echo "Sreedhar:: Hello Prasad, how are you doing?"
+echo "Prasad:: Hey Sreedhar, I am good. Hope you are too."
+echo "Sreedhar:: Hope you have joined and started learning the DevOps course."
+echo "Prasad:: Yes, I just started."
